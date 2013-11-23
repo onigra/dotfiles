369d423f971cf0b2c4e62f5d663a7d89368f3cb3
onigra <3280467rec@gmail.com>
2013-11-24 00:12:54 +0900

    セットアップシェル書いた

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.zsh ~/.zsh
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.inputrc ~/.inputrc
+ln -s ~/dotfiles/.irbrc ~/.irbrc
+
