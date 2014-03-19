313d77bdcfeb975f0e02ee78aae0ea8fe354c7c8
onigra <3280467rec@gmail.com>
2014-03-19 15:56:35 +0900

    rubyのインストール修正

--- ruby_install.sh
+++ ruby_install.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-git clone https://github.com/sstephenson/rbenv.git
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 mkdir -p ~/.rbenv/plugins
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-
-source ~/.zshrc
 
 RUBY_CONFIGURE_OPTS="--enable-shared --with-readline-dir=$(brew --prefix readline) --with-openssl-dir=$(brew --prefix openssl)" rbenv install 2.1.1
 
@@ -12,4 +10,5 @@ rbenv global 2.1.1
 rbenv rehash
 
 gem i bundler pry td rspec powder spring spring-commands-cucumber spring-commands-rspec spring-commands-testunit
+rbenv rehash
 
