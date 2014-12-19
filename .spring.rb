02feb772df121a0081bed0d8fec4c9ed96f2b6ff
onigra <3280467rec@gmail.com>
2014-12-19 16:07:37 +0900

    update

--- .spring.rb
+++
@@ -1,3 +0,0 @@
-require 'spring-commands-rspec'
-require 'spring-commands-testunit'
-require 'spring-commands-cucumber'
--- setup.sh
+++ setup.sh
@@ -2,18 +2,16 @@
 
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 
+ln -s ~/dotfiles/.gemrc ~/.gemrc
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.inputrc ~/.inputrc
+ln -s ~/dotfiles/.irbrc ~/.irbrc
+ln -s ~/dotfiles/.tigrc ~/.tigrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.zsh ~/.zsh
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/.inputrc ~/.inputrc
-ln -s ~/dotfiles/.irbrc ~/.irbrc
-ln -s ~/dotfiles/.gemrc ~/.gemrc
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.tmux-powerlinerc ~/.tmux-powerlinerc
-ln -s ~/dotfiles/.spring.rb ~/.spring.rb
-ln -s ~/dotfiles/Brewfile ~/Brewfile
-ln -s ~/dotfiles/.tigrc ~/.tigrc
 
 mkdir ~/.peco
 ln -s ~/dotfiles/.peco/config.json ~/.peco/config.json
