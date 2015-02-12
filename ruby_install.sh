1893c5bbff2bd07dcd0a1eefc428bf58fade3f2c
onigra <3280467rec@gmail.com>
2015-02-12 17:59:49 +0900

    default-gemsはシンボリックリンクにした

--- ruby_install.sh
+++ ruby_install.sh
@@ -7,8 +7,7 @@ git clone https://github.com/ianheggie/rbenv-binstubs ~/.rbenv/plugins/rbenv-bin
 git clone https://github.com/amatsuda/gem-src.git ~/.rbenv/plugins/gem-src
 git clone https://github.com/sstephenson/rbenv-default-gems.git ~/.rbenv/plugins/rbenv-default-gems
 
-touch ~/.rbenv/default-gems
-echo "bundler\npry\nrspec\nhttparty" > ~/.rbenv/default-gems
+ln -s ~/dotfiles/default-gems ~/.rbenv/default-gems
 
 RUBY_CONFIGURE_OPTS="--enable-shared --with-readline-dir=$(brew --prefix readline) --with-openssl-dir=$(brew --prefix openssl) --with-opt-dir=$(brew --prefix libiconv)" rbenv install 2.1.5
 
