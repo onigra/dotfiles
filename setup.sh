56a1df241d7ba14270bea7b0736c5ad263d6119c
onigra <3280467rec@gmail.com>
2017-03-23 01:34:08 +0900

    zshenv削除

--- setup.sh
+++ setup.sh
@@ -8,7 +8,6 @@ ln -s ~/dotfiles/.inputrc ~/.inputrc
 ln -s ~/dotfiles/.irbrc ~/.irbrc
 ln -s ~/dotfiles/.tigrc ~/.tigrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
-ln -s ~/dotfiles/.zshenv ~/.zshenv
 ln -s ~/dotfiles/.zsh ~/.zsh
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
