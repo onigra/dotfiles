bfdc3e34bf3531171ad89f87b04457a7314da10b
onigra <3280467rec@gmail.com>
2018-06-11 00:15:47 +0900

    pipで入れるものをふやした

--- bin/pip.sh
+++ bin/pip.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 sudo easy_install pip3
-sudo pip3 install ansible
-sudo pip3 install powerline-status
+pip3 install --upgrade pip
+pip3 install ansible
+pip3 install powerline-status
+pip3 install boto
+pip3 install boto3
 
