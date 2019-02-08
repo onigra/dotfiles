bd57e5b13405905d97ffc525be66133a1a36dbb6
onigra <3280467rec@gmail.com>
2019-02-08 17:52:37 +0900

    setupを大修正

--- bin/setup.sh
+++ bin/setup.sh
@@ -9,13 +9,12 @@ ln -s ~/dotfiles/.tigrc ~/.tigrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.zsh ~/.zsh
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/powerline ~/.config/powerline
+ln -s ~/dotfiles/.peco ~/.peco
 
-mkdir ~/.peco
-ln -s ~/dotfiles/.peco/config.json ~/.peco/config.json
-
-mkdir -p ~/.config/git
-ln -s ~/dotfiles/.config/git/ignore ~/.config/git/ignore
+ln -s ~/dotfiles/.config/git ~/.config/git
+ln -s ~/dotfiles/.config/nvim ~/.config/nvim
+ln -s ~/dotfiles/.config/powerline ~/.config/powerline
+ln -s ~/dotfiles/.config/karabiner ~/.config/karabiner
 
 mkdir -p ~/.cache
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
