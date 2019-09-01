d858d48da0cff729d83c10e5e9773205b7d9aaf3
onigra <3280467rec@gmail.com>
2019-09-01 14:48:54 +0900

    LSPのためにrubocop.ymlがないと色々うるさい

--- setup/symlink.sh
+++ setup/symlink.sh
@@ -8,6 +8,7 @@ ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.zsh ~/.zsh
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/.peco ~/.peco
+ln -s ~/dotfiles/.rubocop.yml ~/.rubocop.yml
 
 ln -s ~/dotfiles/.config/git ~/.config/git
 ln -s ~/dotfiles/.config/nvim ~/.config/nvim
