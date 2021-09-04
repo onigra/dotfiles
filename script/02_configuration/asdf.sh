366fea720e1289c29c1f83d7626e9ae79c05b3e2
onigra <3280467rec@gmail.com>
2021-09-04 11:55:12 +0900

    awscliはasdfでインストールする

--- script/02_configuration/asdf.sh
+++ script/02_configuration/asdf.sh
@@ -3,6 +3,7 @@
 asdf plugin-add ruby
 asdf plugin-add nodejs
 asdf plugin-add python
+asdf plugin-add awscli
 
 # asdf plugin-add terraform
 # asdf plugin-add terragrant
--- script/03_packages/python.sh
+++ script/03_packages/python.sh
@@ -5,10 +5,5 @@ pip3 install --upgrade pip
 # https://qiita.com/ksato9700/items/ec30d726a1508c7985a0
 # pip3 install --use-feature=2020-resolver \
   neovim \
-  ansible \
   powerline-status \
-  boto \
-  boto3 \
-  awscli \
   Pygments
-
