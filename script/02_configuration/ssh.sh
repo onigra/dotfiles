763fd7e621c2d22f4b28b14ee0f9e22414b8b9e8
onigra <3280467rec@gmail.com>
2021-09-04 12:15:32 +0900

    sshの基本設定を追加

---
+++ script/02_configuration/ssh.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cp -rp ~/dotfiles/ssh-base ~/.ssh
+
+# ssh-keygen -t ed25519 -C "" -f $HOME/.ssh/github.com/id_ed25519
+# ssh-keygen -t ed25519 -C "" -f $HOME/.ssh/github.com/id_ed25519
