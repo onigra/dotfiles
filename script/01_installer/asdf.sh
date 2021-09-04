431186e29dae250ea6eab71e4b5ce63872a59db1
onigra <3280467rec@gmail.com>
2021-09-04 11:46:28 +0900

    update asdf install script

--- script/01_installer/asdf.sh
+++ script/01_installer/asdf.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf
-cd ~/.asdf
-git checkout "$(git describe --abbrev=0 --tags)"
+# http://asdf-vm.com/guide/getting-started.html#_1-install-dependencies
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.1
