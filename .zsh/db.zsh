1a97bed2ba1b473449668adbbcf3a919d29149ba
onigra <3280467rec@gmail.com>
2026-01-04 23:03:52 +0900

    使ってない

--- .zsh/db.zsh
+++
@@ -1,5 +0,0 @@
-# function something_db() {
-#   ssh -f -N -L 35000:something-db-hostname:5432 $1
-#   psql -h localhost -p 35000 -U foobar -d baz
-#   kill $(ps aux | grep -e '[a]p-northeast-1.rds.amazonaws.com' | awk '{print $2;}')
-# }
