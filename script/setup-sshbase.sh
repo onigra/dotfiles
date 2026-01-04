15089f81485b310a6d78a123769d3806999c502e
onigra <3280467rec@gmail.com>
2026-01-04 17:10:38 +0900

    setup-sshbase

--- script/setup-sshbase.sh
+++ script/setup-sshbase.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cp -rp ~/dotfiles/ssh-base ~/.ssh
+cp -rp $HOME/dotfiles/ssh-base $HOME/.ssh
 
 # ssh-keygen -t ed25519 -C "" -f $HOME/.ssh/github.com/id_ed25519
 # ssh-keygen -t ed25519 -C "" -f $HOME/.ssh/github.com/id_ed25519
